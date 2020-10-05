4bbc599e261f307203b11e1b586558c6eaac362a
hashim <hashimtahir@live.com>
2020-10-05 14:03:44 +0500

    Initial commit
    Signed-off-by: hashim <hashimtahir@live.com>

---
+++ app/src/androidTest/java/com/hashim/spotifyclone/ExampleInstrumentedTest.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020/  10/ 5.  Created by Hashim Tahir
+ */
+
+package com.hashim.spotifyclone
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.hashim.spotifyclone", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/hashim/spotifyclone/MainActivity.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020/  10/ 5.  Created by Hashim Tahir
+ */
+
+package com.hashim.spotifyclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
